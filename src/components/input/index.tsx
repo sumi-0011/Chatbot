85932c245fd2f8aaad9f81011c7aefc735903219
sumi-0011 <selina2000@naver.com>
2023-04-14 00:56:13 +0900

    feat: input, input label component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,37 @@
+import type { ComponentProps } from 'react';
+import styled from 'styled-components';
+
+type InputSizeType = 'md' | 'sm';
+
+export interface InputProps extends ComponentProps<'input'> {
+  inputSize?: InputSizeType;
+}
+
+const INPUT_SIZE: Record<InputSizeType, number> = {
+  md: 72,
+  sm: 65,
+};
+
+function Input(props: InputProps) {
+  const { inputSize, onChange, value, type } = props;
+
+  return (
+    <Wrapper
+      inputSize={inputSize}
+      value={value}
+      type={type}
+      onChange={onChange}
+    />
+  );
+}
+
+const Wrapper = styled.input<{ inputSize?: InputSizeType }>`
+  border: 2px solid #393939;
+
+  border-radius: 5px;
+
+  width: 100%;
+  height: ${({ inputSize }) => INPUT_SIZE[inputSize || 'md']}px;
+`;
+
+export default Input;
