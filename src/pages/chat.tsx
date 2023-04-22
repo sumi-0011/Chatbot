0f3806552a42174c8d7d20094271b2979848faac
sumi-0011 <selina2000@naver.com>
2023-04-22 21:21:33 +0900

    feat: chat hook 생성

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,23 +1,40 @@
+import { useEffect, useRef } from 'react';
+
+import Button from '@/components/button';
+import Input from '@/components/input';
 import ReceiveMessage from '@/components/message/receiver';
 import SenderMessage from '@/components/message/sender';
+import useChat from '@/hooks/useChat';
 
 function ChatPage() {
+  const { submitChat, messages } = useChat(4);
+  console.log('messages: ', messages);
+  const sendMessage = useRef('');
+  useEffect(() => {
+    // submitChat('안녕하세요');
+  }, []);
+
   return (
     <div>
-      <SenderMessage
-        img="/assets/images/smiling1.png"
-        name="chat1"
-        message={
-          'ㄴㄹㅁㄹㄴㅇqweqweqweqweqweqweqwe럼ㅇ린어리너린아ㅓ린아러ㅣㄴㄹ'
-        }
-        date={'2021-08-01'}
-      />
+      {messages.map((message, idx) => {
+        return (
+          <div key={idx}>
+            {message.author === 0 ? (
+              <ReceiveMessage message={message.content} />
+            ) : (
+              <SenderMessage
+                img="/assets/images/smiling1.png"
+                name="chat1"
+                message={message.content}
+                date={message.createdAt}
+              />
+            )}
+          </div>
+        );
+      })}
 
-      <ReceiveMessage
-        message={
-          'ㄴㄹㅁㄹㄴㅇqweqweqweqweqweqweqwe럼ㅇ린어리너린아ㅓ린아러ㅣㄴㄹ'
-        }
-      />
+      <Input onChange={(e) => (sendMessage.current = e.target.value)} />
+      <Button onClick={() => submitChat(sendMessage.current)}>전송</Button>
     </div>
   );
 }
