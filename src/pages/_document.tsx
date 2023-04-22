deda7e248c96cfb86f1d3230d8faaa66379c57cc
sumi-0011 <selina2000@naver.com>
2023-04-23 02:39:34 +0900

    feat: add meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,24 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="shortcut icon" href="/assets/images/numble.svg" />
+          <link rel="apple-touch-icon" href="/assets/images/numble.svg" />
+
+          <meta name="description" content="AI Chat Room" />
+          <meta name="og:title" content="AI Chat Room" />
+          <meta name="og:description" content="AI Chat Room" />
+          <meta name="og:image" content="/assets/images/numble.svg" />
+          <meta name="og:url" content="https://numble.vercel.app" />
+          <meta name="og:type" content="website" />
+          <meta name="og:site_name" content="AI Chat Room" />
+          <meta name="og:locale" content="ko_KR" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content="AI Chat Room" />
+          <meta name="twitter:description" content="AI Chat Room" />
+          <meta name="twitter:image" content="/assets/images/numble.svg" />
+          <meta name="twitter:url" content="https://numble.vercel.app" />
         </Head>
         <body>
           <Main />
